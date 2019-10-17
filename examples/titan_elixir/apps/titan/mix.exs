d89cbcfedaaa2b228ffd878817a6d123a7b835e9
zhongwencool <zhongwencool@gmail.com>
2019-10-17 15:05:58 +0800

    update 0.3.0

--- examples/titan_elixir/apps/titan/mix.exs
+++ examples/titan_elixir/apps/titan/mix.exs
@@ -29,7 +29,7 @@ defmodule Titan.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
-      {:ecron, ">= 0.2.0"}
+      {:ecron, ">= 0.3.0"}
     ]
   end
 end
